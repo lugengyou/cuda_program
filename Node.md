262de86703d150ead64e945da0309990426163c4
lugy <lugengyou@github.com>
2024-05-11 11:21:03 +0800

    添加常用命令笔记

---
+++ Node.md
@@ -0,0 +1,20 @@
+# 命令行编译
+```
+# 简单编程程序
+nvcc hello.cu -o hello
+
+
+
+```
+
+# 常用命令
+```
+# 查看 nvcc 所在位置
+watch nvcc
+
+# 查看本机设备数量
+ls -l /dev/nv*
+
+```
+
+
--- helloWorld/hello.cu
+++ helloWorld/hello.cu
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 __global__ void helloFromGPU(void) {
-
-    printf("Hello world from GPU.\n");
+    int i = threadIdx.x;
+    printf("Hello world from GPU by thread %d.\n", i);
 }
-
 
 int main(void) {
 
     printf("Hello world from CPU.\n\n");
 
     helloFromGPU<<<1, 10>>>();
-    cudaDeviceReset();
+    // cudaDeviceReset();
+    cudaDeviceSynchronize();
 
     return 0;
 }
